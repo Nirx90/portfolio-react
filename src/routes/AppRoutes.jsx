01552f482968446ac4c15b386c0e8192cde85e56
Nirx90 <chaudharinirav2001@gmail.com>
2025-09-13 16:46:39 +0530

    page route updated

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Contact from "../pages/Contact";
 import Theme from "../pages/Theme";
-import PortfolioHomepage from "../pages/Home";
 import Inquiry from "../pages/Inquiry";
 import Profile from "../pages/Profile";
+import Home from "../pages/Home";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<PortfolioHomepage />} />
+      <Route path="/" element={<Home />} />
       <Route path="/theme" element={<Theme />} />
       <Route path="/inquiry" element={<Inquiry />} />
       <Route path="/profile" element={<Profile />} />
